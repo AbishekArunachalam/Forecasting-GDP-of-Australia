3d351a558f3f51290f47ad326ef6fa92ecd68039
abi005 <37197857+abi005@users.noreply.github.com>
2018-04-29 16:30:59 +1000

    Add files via upload

---
+++ Balance_On_Goods_And_Services.R
@@ -0,0 +1,37 @@
+library(readxl)
+library(dplyr)
+library(lubridate)
+Balance_on_goods_and_services <- read_excel("Balance on goods and services (5368m) Column B.xls", 
+                                            sheet = "Data1", col_types = c("date", 
+                                                                           "text", "text", "text", "text", "text", 
+                                                                           "text", "text", "text", "text", "text", 
+                                                                           "text", "text", "text", "text", "text"))
+
+# create data-frame with desired rows and columns from original dataset
+mydata <- Balance_on_goods_and_services[-c(1:9),-c(3:16)]
+str(mydata) # Balance is in char format and date is in POSIXct format
+names(mydata)
+# Rename columns
+colnames(mydata) <- c("Date","Balance_on_goods_and_services")
+
+mydata$Date <- as.Date(mydata$Date) #Convert to date format
+mydata$Balance_on_goods_and_services <- as.numeric(mydata$Balance_on_goods_and_services) # Convert Balance to numeric
+
+qtrDate <- quarter(mydata$Date,with_year = T,fiscal_start = 1) #Allocate months to quarters(Lubridate package)
+qtrDate <- gsub(".1","-Q1",qtrDate,fixed = T) #Substitute quarter numbers to explicit characters
+qtrDate <- gsub(".2", "-Q2", qtrDate, fixed = T)
+qtrDate <- gsub(".3", "-Q3", qtrDate, fixed = T)
+qtrDate <- gsub(".4", "-Q4", qtrDate, fixed = T)
+mydata <- mydata %>% mutate(qtrDate) #Add new column containing quarter to data frame 
+mydata <- mydata[,-1] #Remove date column
+mydata <- mydata %>% group_by(qtrDate) %>% summarize(Balance_on_goods_and_services = sum(Balance_on_goods_and_services)) #Group by year quarters and calculate sum
+Balance_on_goods_and_services <- separate(mydata, qtrDate, into = c("Year", "Quarter"), sep="-") #Split quarter year column into two. One for year and other for quarter. 
+colnames(Balance_on_goods_and_services)[3] <- "Balance_on_goods_and_services($ Million)"
+View(Balance_on_goods_and_services)
+write.csv(file = "balance_on_goods_and_services.csv",Balance_on_goods_and_services)
+
+
+
+
+
+
